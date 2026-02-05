806b907e7c5922e3858592b6077f85b4f8c9ff95
technofan900 <technofan900@gmail.com>
2026-02-05 14:56:09 +0200

    fixed validation with help of skolotajs

--- Http/controllers/register/create.php
+++ Http/controllers/register/create.php
@@ -10,38 +10,38 @@ $username = $_POST['username'] ?? '';
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 
-// $errors = [];
+$errors = [];
 
-// $_SESSION['old'] = [
-//     'email' => $email
-// ];
+$_SESSION['old'] = [
+    'email' => $email
+];
 
-// if (! Validator::email($email)) {
-//     $errors['email'] = "Please provide a valid email address.";
-// }
+if (! Validator::email($email)) {
+    $errors['email'] = "Please provide a valid email address.";
+}
 
-// $passw_min_ln = 4;
-// $passw_max_ln = 255;
+$passw_min_ln = 4;
+$passw_max_ln = 255;
 
-// if (! Validator::string($password, $passw_min_ln, $passw_max_ln)) {
-//     $errors['password'] = "Please provide a password at least {$passw_min_ln} characters.";
-// }
+if (! Validator::string($password, $passw_min_ln, $passw_max_ln)) {
+    $errors['password'] = "Please provide a password at least {$passw_min_ln} characters.";
+}
 
-// if (! empty($errors)) {
-//     $_SESSION['errors'] = $errors;
-//     redirect('/register'); 
-// }
+if (! empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    redirect('/register'); 
+}
 
-// $user = $db->query('SELECT * FROM users WHERE email = :email', [
-//     'email' => $email
-// ])->find();
+$user = $db->query('SELECT * FROM login WHERE email = :email', [
+    'email' => $email
+])->find();
 
-// if ($user) {
-//     $_SESSION['errors'] = [
-//         'email' => 'Email already registered. Please log in.'
-//     ];
-//     redirect('/login');
-// }
+if ($user) {
+    $_SESSION['errors'] = [
+        'email' => 'Email already registered. Please log in.'
+    ];
+    redirect('/login');
+}
 
 $db->query('INSERT INTO login (username, email, password) VALUES (:username, :email, :password)', [
 	'username' => $username,

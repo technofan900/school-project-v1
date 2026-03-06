5274767b38aaf0d457c977e6e17ca6c2b0347b34
technofan900 <technofan900@gmail.com>
2026-03-06 18:56:40 +0200

    added updated sql database

--- extra/school_project.sql
+++ extra/school_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2026 at 11:15 PM
+-- Generation Time: Mar 06, 2026 at 05:55 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `school_project`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `folders`
+--
+
+CREATE TABLE `folders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `folder_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `folders`
+--
+
+INSERT INTO `folders` (`id`, `user_id`, `folder_name`, `created_at`) VALUES
+(1, 7, 'testing', '2026-03-06 15:14:20');
 
 -- --------------------------------------------------------
 
@@ -42,9 +62,10 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`) VALUES
 (1, 'test', '1@1', '$2y$10$nxIeSYvnT38DpnAZA4WvH.enmMX63314q7yJ87hebD4Gs/C5NTdIG', '2026-02-01 17:01:23', '2026-02-01 17:01:23'),
-(2, 'q', '2@2', '$2y$10$CIjXDmWs95fISEzJc0lCcuqBMRux9mSnj5ni26iB6sMND.F0ifCA6', '2026-02-03 15:24:29', '2026-02-03 15:24:29'),
-(3, 'test', '1@4', '$2y$10$sYsQtxErKJIyMZKdo095JOw2CyHwtuDQWSqA13oZscTB8lOEL8pUW', '2026-02-03 15:54:23', '2026-02-03 15:54:23'),
-(4, 'test', '1@1.com', '$2y$10$VrZ42JpUDNZIe0cd3c1pQupfHp.L8AvOsetDukK3REFW1Flpo5aky', '2026-02-11 16:29:30', '2026-02-11 16:29:30');
+(4, 'test', '1@1.com', '$2y$10$VrZ42JpUDNZIe0cd3c1pQupfHp.L8AvOsetDukK3REFW1Flpo5aky', '2026-02-11 16:29:30', '2026-02-11 16:29:30'),
+(5, 'qwerty', 'qwer@ty.com', '$2y$10$hpNMAnNzePwwwbPeuy8LAO3ca4TyuSsTYbh6bLmF0jCJltyyFysma', '2026-03-03 18:56:17', '2026-03-03 18:56:17'),
+(6, 'qwt', '1@2.com', '$2y$10$3ijl7aVV2RTSRNPSatz8b.hTU/zSeHcTNrjv0MkyiplbOGLTrlUy2', '2026-03-03 18:58:00', '2026-03-03 18:58:00'),
+(7, '12qw', 'q@w.lv', '$2y$10$hM/iz.YeUGAITJCvYZlBIeEBmIwxEqqdqnqEmPi2Mry0wNMN7ZVNy', '2026-03-04 19:13:33', '2026-03-04 19:13:33');
 
 -- --------------------------------------------------------
 
@@ -57,20 +78,33 @@ CREATE TABLE `passwords` (
   `name` varchar(255) NOT NULL,
   `login_data` text NOT NULL,
   `password` varchar(255) NOT NULL,
-  `userID` int(11) DEFAULT NULL
+  `userID` int(11) DEFAULT NULL,
+  `folder_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `passwords`
 --
 
-INSERT INTO `passwords` (`id`, `name`, `login_data`, `password`, `userID`) VALUES
-(1, 'test', 'qwerty', '12345', 1),
-(2, 'dati', 'qwerty', '123456', 1);
+INSERT INTO `passwords` (`id`, `name`, `login_data`, `password`, `userID`, `folder_id`) VALUES
+(1, 'test', 'qwerty', '12345', 1, NULL),
+(2, 'dati', 'qwerty', '123456', 1, NULL),
+(3, 'weeds', 'qwerty1', '1234567', 1, NULL),
+(6, 'testds', 'dooods', 'qwerty1', 5, NULL),
+(7, 'pass1', '1123', '1234', 7, NULL),
+(8, 'pass2', '1234rt', '56789o', 7, 1),
+(9, 'pass3', 'qwedf', '12345t', 7, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `folders`
+--
+ALTER TABLE `folders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `login`
@@ -84,33 +118,47 @@ ALTER TABLE `login`
 --
 ALTER TABLE `passwords`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `userID` (`userID`);
+  ADD KEY `userID` (`userID`),
+  ADD KEY `folder_id` (`folder_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `folders`
+--
+ALTER TABLE `folders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `passwords`
 --
 ALTER TABLE `passwords`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `folders`
+--
+ALTER TABLE `folders`
+  ADD CONSTRAINT `folders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `passwords`
 --
 ALTER TABLE `passwords`
-  ADD CONSTRAINT `passwords_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `login` (`id`);
+  ADD CONSTRAINT `passwords_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `login` (`id`),
+  ADD CONSTRAINT `passwords_ibfk_2` FOREIGN KEY (`folder_id`) REFERENCES `folders` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
